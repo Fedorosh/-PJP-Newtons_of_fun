587aa8ea0d906685f8a7884488eff1fff1b187d0
Janosz Zieliński <32450917+Fedorosh@users.noreply.github.com>
2017-11-24 14:50:51 +0100

    Update main.cpp

--- physicsXd/main.cpp
+++ physicsXd/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <freeglut.h>
-#include <glut.h>
-
+#include "Dependencies\freeglut\freeglut.h"
+#include "Dependencies\glew\glew.h"
 using namespace std;
 
 int main(int argc, char **argv)
 {
-	cout << "hi world !";
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(500, 500);//optional
+	glutInitWindowSize(800, 600); //optional
+	glutCreateWindow("OpenGL First Window");
+
+	glutMainLoop();
 	return 0;
 }
