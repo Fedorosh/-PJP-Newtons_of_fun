bfef79dd40b2a030d309e98053d81abddc5171bb
Fedorosh <Vapaiss1@gmail.com>
2017-11-26 00:33:22 +0100

    opengl

--- physicsXd/main.cpp
+++ physicsXd/main.cpp
@@ -3,6 +3,15 @@
 #include <freeglut.h>
 using namespace std;
 
+void renderScene(void)
+{
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(1.0, 0.0, 0.0, 1.0);//clear red
+
+	glutSwapBuffers();
+}
+
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
@@ -10,6 +19,17 @@ int main(int argc, char **argv)
 	glutInitWindowPosition(500, 500);//optional
 	glutInitWindowSize(800, 600); //optional
 	glutCreateWindow("Dzia³a kuurrwa");
+
+	glewInit();
+	if (glewIsSupported("GL_VERSION_6_14"))
+	{
+		cout << "wspiera";
+	}
+	else cout << "nie wspiera";
+
+	glEnable(GL_DEPTH_TEST);
+
+	glutDisplayFunc(renderScene);
 
 	glutMainLoop();
 	return 0;
