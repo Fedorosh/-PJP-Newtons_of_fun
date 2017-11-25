4a741d5df8cab962aeedb2ff489440614f450d23
Fedorosh <Vapaiss1@gmail.com>
2017-11-25 23:33:53 +0100

    Naprawienie opengla

--- physicsXd/main.cpp
+++ physicsXd/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "Dependencies\freeglut\freeglut.h"
-#include "Dependencies\glew\glew.h"
+#include <glew.h>
+#include <freeglut.h>
 using namespace std;
 
 int main(int argc, char **argv)
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(500, 500);//optional
 	glutInitWindowSize(800, 600); //optional
-	glutCreateWindow("OpenGL");
+	glutCreateWindow("Dzia³a kuurrwa");
 
 	glutMainLoop();
 	return 0;
