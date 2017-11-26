fa7506f13a5056ea57781a2cd111c7e97b93d07a
Fedorosh <Vapaiss1@gmail.com>
2017-11-26 12:50:54 +0100

    triangle

--- physicsXd/main.cpp
+++ physicsXd/main.cpp
@@ -1,37 +1,46 @@
-#include <iostream>
-#include <glew.h>
-#include <freeglut.h>
-#include <wglew.h>
+#include "Core\Shader_Loader.h"
+
 using namespace std;
+using namespace Core;
+
+GLuint program;
 
 void renderScene(void)
 {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(1.0, 1.0, 1.0, 0.0);//clear red
+	glClearColor(1.0, 0.0, 0.0, 1.0);
+
+	glUseProgram(program);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	glutSwapBuffers();
+}
+
+void Init()
+{
+	glEnable(GL_DEPTH_TEST);
+
+	Core::Shader_Loader shaderLoader;
+	program = shaderLoader.CreateProgram("Shaders\\Vertex_Shader.glsl", "Shaders\\Fragment_Shader.glsl");
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(500, 500);//optional
-	glutInitWindowSize(800, 600); //optional
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 600); 
 	glutCreateWindow("Dzia³a kuurrwa");
-
 	glewInit();
-	if (glewIsSupported("GL_VERSION_1_4"))
-	{
-		cout << "wspiera";
-	}
-	else cout << "nie wspiera";
 
-	glEnable(GL_DEPTH_TEST);
+	Init();
 
 	glutDisplayFunc(renderScene);
-
 	glutMainLoop();
+	glDeleteProgram(program);
 	return 0;
 }
